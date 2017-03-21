0ef9e27518127fe949124d2e38fee1f742f14442
Taehupo <uzumakigip@gmail.com>
2017-03-21 15:19:11 +0100

    Ajout du fichier game.c et game.h

---
+++ game.c
@@ -0,0 +1,8 @@
+#include <gtk/gtk.h>
+
+#include "game.h"
+
+void game_init()
+{
+	cannon_sprite = cairo_image_surface_create_from_png("resource/Cannon.png")
+}
---
+++ game.h
@@ -0,0 +1,11 @@
+#ifndef GAME_H
+#define GAME_H
+
+typedef struct
+{
+	cairo_surface_t * cannon_sprite;
+}Game;
+
+void game_init();
+
+#endif
